836012cc9b2a49181fce3fb6d5270c568ab70933
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2025-02-17 22:22:56 -0300

    fix: alterando nome do arquivo

--- TechLibrary.Communication/Requests/RequestRegisterUserJson.cs
+++ TechLibrary.Communication/Requests/RequestRegisterUserJson.cs
@@ -1,5 +1,5 @@
 ﻿namespace TechLibrary.Communication.Requests;
-public class RequestUserJson
+public class RequestRegisterUserJson
 {
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
